38e51756bcf8f31a69073048456102fa5c83bdb5
Utkarsh Singh <uks08122002.com>
2025-04-29 14:03:28 +0530

    last2

--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -1,11 +1,11 @@
 "use client";
 import axios from "axios";
 
-// export const axiosClient = axios.create({
-//   baseURL:process.env.NEXT_PUBLIC_BASE_URL,
-//   withCredentials: true,
-// });
 export const axiosClient = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL:process.env.NEXT_PUBLIC_BASE_URL,
   withCredentials: true,
 });
+// export const axiosClient = axios.create({
+//   baseURL: "http://localhost:3000",
+//   withCredentials: true,
+// });
